3b8bf49ea0bbb3cb23a1e923265016e67ff380d7
svkanoria <saumya.kanoria@gmail.com>
2025-11-09 22:11:15 +0530

    Adds nuqs and more ui components

--- app/topCustomers/page.tsx
+++ app/topCustomers/page.tsx
@@ -1,4 +1,5 @@
 import { Heading } from "@/components/typography/heading";
+import { Combobox } from "@/components/ui/combobox";
 import { DatePicker } from "@/components/ui/datePicker";
 import { getTopCustomersByVolume } from "@/lib/api";
 
@@ -10,6 +11,16 @@ export default async function Page() {
       <Heading level="h1">Top Customers</Heading>
       <div className="p-4 border mb-4">
         <DatePicker />
+        <Combobox
+          options={[
+            { value: "Formaldehyde", label: "Formaldehyde" },
+            { value: "Formaldehyde-37%", label: "Formaldehyde-37%" },
+            { value: "Formaldehyde-40%", label: "Formaldehyde-40%" },
+            { value: "Formaldehyde-41%", label: "Formaldehyde-41%" },
+            { value: "Formaldehyde-43%", label: "Formaldehyde-43%" },
+            { value: "Formaldehyde-36.5%", label: "Formaldehyde-36.5%" },
+          ]}
+        />
       </div>
       <ul>
         {data.map((row) => (
--- components/ui/datePicker.tsx
+++ components/ui/datePicker.tsx
@@ -6,7 +6,6 @@ import { CalendarIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Popover,
   PopoverContent,
